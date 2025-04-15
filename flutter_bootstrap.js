9dde1aef004cf217b8723733eeebffa6a867f5e7
shivansh-gupta-08 <shivansh.gupta@tide.co>
2025-04-15 11:50:13 +0530

    week 3

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1841578701"
+    serviceWorkerVersion: "2687740447"
   }
 });
